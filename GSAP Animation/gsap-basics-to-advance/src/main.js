a48c0235eeb18916a97d3489b3e6727bad46d4f4
DhruuvShah <dhruv27shah@gmail.com>
2026-09-22 20:28:03 +0530

    Implement fade-in effect for box element using GSAP

--- GSAP Animation/gsap-basics-to-advance/src/main.js
+++ GSAP Animation/gsap-basics-to-advance/src/main.js
@@ -1,10 +1,13 @@
-import './style.css'
+import "./style.css";
 import gsap from "gsap";
 
-
-gsap.to(".box", {
-  x: 300,        // move 300px right
-  rotation: 360, // spin a full turn
-  duration: 2,   // over 2 seconds
-  ease: "power2.out"
-});
+gsap.fromTo(
+  ".box",
+  {
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    duration: 1,
+  },
+);

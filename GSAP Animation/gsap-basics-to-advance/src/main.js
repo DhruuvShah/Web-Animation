be69fa7f63f8b5365ae9a9a76cf36f4c4032c993
DhruuvShah <dhruv27shah@gmail.com>
2026-09-25 10:54:27 +0530

    Refactor HTML structure and implement loader and hero section animations using GSAP

--- GSAP Animation/gsap-basics-to-advance/src/main.js
+++ GSAP Animation/gsap-basics-to-advance/src/main.js
@@ -1,30 +1,64 @@
 import "./style.css";
 import gsap from "gsap";
 
-const tl = gsap.timeline();
+const obj = {
+  value: 0,
+};
 
-tl.to(".box1", {
-  x: 1000,
-  duration: 1.3,
-  ease: "power3.out",
+const counter = document.querySelector(".loader-count h2");
+gsap.to(obj, {
+  value: 100,
+  duration: 1.7,
+  ease: "none",
+  onUpdate: () => {
+    counter.textContent = `${Math.round(obj.value)}%`;
+  },
+  onComplete: () => {
+    gsap.to(counter, {
+      opacity: 0,
+      duration: 1.2,
+      ease: "power3.out",
+      onComplete: () => {
+        tl.play();
+      },
+    });
+  },
+});
+
+const tl = gsap.timeline({ paused: true });
+
+gsap.set([".heading h1", ".sub-heading p"], {
+  yPercent: 110,
+});
+
+tl.to(".loader", {
+  yPercent: 100,
+  duration: 1.2,
+  ease: "expo.out",
 })
-  .to(".box2", {
-    x: 1000,
-    duration: 1.3,
-    ease: "power3.out",
-  })
-  .to(".box3", {
-    x: 1000,
-    duration: 1.3,
-    ease: "power3.out",
-  }, 0)
-  .to(".box4", {
-    x: 1000,
-    duration: 1.3,
-    ease: "power3.out",
-  })
-  .to(".box5", {
-    x: 1000,
-    duration: 1.3,
-    ease: "power3.out",
-  });
+  .from(
+    ".hero-bg img",
+    {
+      scale: 1.5,
+      duration: 1.3,
+      ease: "expo.out",
+    },
+    "-=1.1",
+  )
+  .to(
+    ".heading h1",
+    {
+      yPercent: 0,
+      duration: 1.1,
+      ease: "power3.out",
+    },
+    "-=0.9",
+  ).to(
+    ".sub-heading p",
+    {
+      yPercent: 0,
+      duration: 1.1,
+      ease: "power3.out",
+    },
+    "-=0.8",
+  );
